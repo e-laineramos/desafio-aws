0cec2dc7b281cf120beaefb17c80ca4da88b5524
Elaine Ramos <correa.elaineramos@gmail.com>
2025-03-09 20:38:30 -0300

    Desafio AWS

---
+++ test/desafio-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DesafioAws from '../lib/desafio-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/desafio-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DesafioAws.DesafioAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
